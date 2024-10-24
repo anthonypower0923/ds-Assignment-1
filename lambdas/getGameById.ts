bf8b3ce03e80a15ad2146f75a2c9ee7a2562452f
anthonypower0923 <20098384@mail.wit.ie>
2024-10-24 11:52:15 +0100

    have lambda communicating with dynamoDB

---
+++ lambdas/getGameById.ts
@@ -0,0 +1,74 @@
+import { Handler } from "aws-lambda";
+
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb";
+// Initialization
+const ddbDocClient = createDDbDocClient();
+// Handler
+export const handler: Handler = async (event, context) => {
+  try {
+    console.log("Event: ", JSON.stringify(event));
+    const parameters = event?.queryStringParameters;
+    const gameId = parameters ? parseInt(parameters.gameId) : undefined;
+
+    if (!gameId) {
+      return {
+        statusCode: 404,
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ Message: "Missing game Id" }),
+      };
+    }
+    const commandOutput = await ddbDocClient.send(
+      new GetCommand({
+        TableName: process.env.TABLE_NAME,
+        Key: { id: gameId },
+      })
+    );
+    if (!commandOutput.Item) {
+      return {
+        statusCode: 404,
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ Message: "Invalid game Id" }),
+      };
+    }
+    const body = {
+      data: commandOutput.Item,
+    };
+
+    // Return Response
+    return {
+      statusCode: 200,
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(body),
+    };
+  } catch (error: any) {
+    console.log(JSON.stringify(error));
+    return {
+      statusCode: 500,
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ error }),
+    };
+  }
+};
+
+function createDDbDocClient() {
+  const ddbClient = new DynamoDBClient({ region: process.env.REGION });
+  const marshallOptions = {
+    convertEmptyValues: true,
+    removeUndefinedValues: true,
+    convertClassInstanceToMap: true,
+  };
+  const unmarshallOptions = {
+    wrapNumbers: false,
+  };
+  const translateConfig = { marshallOptions, unmarshallOptions };
+  return DynamoDBDocumentClient.from(ddbClient, translateConfig);
+}
--- lib/assignment-app-stack.ts
+++ lib/assignment-app-stack.ts
@@ -19,6 +19,33 @@ export class AssignmentAppStack extends cdk.Stack {
       tableName: "Games",
     });
 
+    const getGameByIdFn = new lambdanode.NodejsFunction(
+      this,
+      "GetGameByIdFn",
+      {
+        architecture: lambda.Architecture.ARM_64,
+        runtime: lambda.Runtime.NODEJS_18_X,
+        entry: `${__dirname}/../lambdas/getGameById.ts`,
+        timeout: cdk.Duration.seconds(10),
+        memorySize: 128,
+        environment: {
+          TABLE_NAME: gamesTable.tableName,
+          REGION: 'eu-west-1',
+        },
+      }
+    );
+
+    const getGameByIdURL = getGameByIdFn.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+      cors: {
+        allowedOrigins: ["*"],
+      },
+    });
+
+    gamesTable.grantReadData(getGameByIdFn)
+
+    new cdk.CfnOutput(this, "Get Movie Function Url", { value: getGameByIdURL.url });
+
     new custom.AwsCustomResource(this, "gamesddbInitData", {
       onCreate: {
         service: "DynamoDB",
